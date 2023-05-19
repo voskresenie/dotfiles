0f70032f86ad3f2bfd32618c78e4dfdd99bf3be2
David Anderson <voskresno@gmail.com>
2023-05-19 17:44:59 -0500

    Revamp shell profiles

--- cfg/system/profile.d/user.sh
+++ cfg/system/profile.d/user.sh
@@ -2,8 +2,4 @@
 #
 # This file contains global default env vars
 #
-export EDITOR=nvim
-export XDG_CONFIG_HOME="$HOME"/.config
-export XDG_CACHE_HOME="$HOME"/.cache
-export XDG_DATA_HOME="$HOME"/.local/share
-export XDG_STATE_HOME="$HOME"/.local/state
+listop prepend PROFILES /etc/profile.d/user.sh
